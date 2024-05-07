c58dc76acadef2c612cc13e7e62cc5e64be678be
nguyen <nguyenanthuan@gmail.com>
2024-05-07 16:04:15 +0700

    build

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Reliable cron job service - FastCron';
+export const SITE_DESCRIPTION = 'You can create and manage cronjobs for multiple websites, with your team. Know when your cron jobs are executed, get failure notifications via email, Slack, and webhooks.';
